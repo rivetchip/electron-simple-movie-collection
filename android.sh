741c61c826297ecc1c1927f98150901c017b8da8
rivetchip <me@spidery.fr>
2018-06-16 14:51:13 +0200

    update script folders

--- android.sh
+++ android.sh
@@ -2,22 +2,27 @@
 
 set -e
 
-AAPT="/path/to/android-sdk/build-tools/23.0.3/aapt"
-DX="/path/to/android-sdk/build-tools/23.0.3/dx"
-ZIPALIGN="/path/to/android-sdk/build-tools/23.0.3/zipalign"
-APKSIGNER="/path/to/android-sdk/build-tools/26.0.1/apksigner" # /!\ version 26
-PLATFORM="/path/to/android-sdk/platforms/android-19/android.jar"
+cd android
+
+SDK="/home/fox/Android/Sdk"
+
+AAPT="$SDK/build-tools/28.0.0/aapt"
+DX="$SDK/build-tools/28.0.0/dx"
+ZIPALIGN="$SDK/build-tools/28.0.0/zipalign"
+APKSIGNER="$SDK/build-tools/28.0.0/apksigner"
+PLATFORM="$SDK/platforms/android-28/android.jar"
+
 
 echo "Cleaning..."
 rm -rf obj/*
-rm -rf src/com/example/helloandroid/R.java
+rm -rf java/fr/spidery/moviecollection/R.java
 
 echo "Generating R.java file..."
-$AAPT package -f -m -J src -M AndroidManifest.xml -S res -I $PLATFORM
+$AAPT package -f -m -J java -M AndroidManifest.xml -S res -I $PLATFORM
 
-echo "Compiling..."
-javac -d obj -classpath src -bootclasspath $PLATFORM -source 1.7 -target 1.7 src/com/example/helloandroid/MainActivity.java
-javac -d obj -classpath src -bootclasspath $PLATFORM -source 1.7 -target 1.7 src/com/example/helloandroid/R.java
+echo "Compiling..." #java-1.8.0-openjdk-devel
+javac -d obj -classpath java -bootclasspath $PLATFORM -source 1.7 -target 1.7 java/fr/spidery/moviecollection/MainActivity.java
+javac -d obj -classpath java -bootclasspath $PLATFORM -source 1.7 -target 1.7 java/fr/spidery/moviecollection/R.java
 
 echo "Translating in Dalvik bytecode..."
 $DX --dex --output=classes.dex obj
@@ -33,5 +38,12 @@ $ZIPALIGN -f 4 bin/hello.unaligned.apk bin/hello.apk
 if [ "$1" == "test" ]; then
 	echo "Launching..."
 	adb install -r bin/hello.apk
-	adb shell am start -n com.example.helloandroid/.MainActivity
+	adb shell am start -n fr.spidery.moviecollection/.MainActivity
 fi
+
+
+
+
+
+
+
