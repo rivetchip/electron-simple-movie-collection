e6d0a8748685d3c06248fd8839b9a78b6bfdb081
rivetchip <me@spidery.fr>
2018-07-08 03:03:01 +0200

    add utils func

---
+++ application/utils.js
@@ -0,0 +1,126 @@
+
+/**
+ * Loop over an onject
+ * @param {Object} object 
+ * @param {Function} iteratee 
+ */
+function objectEach(object, iteratee) { // lodash
+    object = Object(object)
+
+    return Object.keys(object).forEach((key) => iteratee(object[key], key))
+}
+
+/**
+ * Loop over and array
+ * @param {Array} array 
+ * @param {Function} iteratee 
+ */
+function arrayEach(array, iteratee) { // lodash
+    return array.forEach((value, key) => iteratee(value, key))
+}
+
+/**
+ * Loop over an object or array
+ * @param {Array|Object} collection 
+ * @param {Function} iteratee 
+ */
+export function forEach(collection, iteratee) {
+    const func = Array.isArray(collection) ? arrayEach : objectEach
+
+    return func(collection, iteratee)
+}
+
+/**
+ * Map over an object
+ * @param {Object} object 
+ * @param {Function} iteratee 
+ */
+function objectMap(object, iteratee) { // lodash
+    object = Object(object)
+
+    const result = {}
+
+    Object.keys(object).forEach((key) => {
+        result[key] = iteratee(object[key], key)
+    })
+    return result
+}
+
+/**
+ * Map over an array
+ * @param {Array} array 
+ * @param {Function} iteratee 
+ */
+function arrayMap(array, iteratee) { // lodash
+    return array.map(iteratee)
+}
+
+/**
+ * Map over an object or array
+ * @param {Object|Array} collection 
+ * @param {Function} iteratee 
+ */
+export function map(collection, iteratee) {
+    const func = Array.isArray(collection) ? arrayMap : objectMap
+
+    return func(collection, iteratee)
+}
+
+/**
+ * String replace with {key: value}
+ * @param {String} string 
+ * @param {Object} replaces 
+ */
+export function replace(string, replaces) {
+
+    forEach(replaces, (value, key) => {
+        string = string.replace(key, value)
+    })
+
+    return string
+}
+
+
+/**
+ * Check if object is empty
+ * @param {Object} object 
+ */
+function objectIsEmpty(object) {
+    return !Object.keys(object).length
+}
+
+/**
+ * Check if parameter can be empty
+ * @param {any} value 
+ */
+function genericIsEmpty(value) {
+    return !value.length
+}
+
+/**
+ * Check if an object or array is empty (has values)
+ * @param {Object|Array} value 
+ */
+export function isEmpty(value) { // lodash
+    const func = Array.isArray(value) ? genericIsEmpty : objectIsEmpty
+
+    return func(value)
+}
+
+/**
+ * Return a query string based on an object {key: value}
+ * FIXME does not work with arrays, only object
+ * @param {Object} object 
+ */
+export function urlstringify(object) {
+    let escape = encodeURIComponent
+    let params = []
+
+    forEach(object, (value, key) => {
+        params.push(key + '=' + escape(value))
+    })
+
+    return params.join('&')
+}
+
+
