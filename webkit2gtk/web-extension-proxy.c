7381f49b54f807401682148b64591dc60cda6fce
rivetchip <me@spidery.fr>
2019-01-06 01:42:44 +0100

    add basic methods

--- webkit2gtk/web-extension-proxy.c
+++ webkit2gtk/web-extension-proxy.c
@@ -2,89 +2,70 @@
 #include <webkit2/webkit-web-extension.h>
 #include <JavaScriptCore/JavaScript.h>
 
-JSClassRef WebAppInterface();
 
+static JSCValue* javascriptinterface_open_collection(WebKitWebExtension *extension) {
 
+    JSCContext *context = jsc_context_get_current();
 
-static char *readFile(char *filename) {
-    char *buffer = 0;
-    long length;
+    GError *error_read = NULL;
 
-    FILE *f = fopen(filename, "r");
+    char *content = NULL;
+    g_file_get_contents("/home/fox/Bureau/vscode", &content, NULL, &error_read);
 
-    if(f) {
-        fseek(f, 0, SEEK_END);
-        length = ftell(f);
-    
-        fseek(f, 0, SEEK_SET);
-        buffer = malloc(length);
-
-        if(buffer) {
-            fread(buffer, 1, length, f);
-        }
-    
-        fclose(f);
-        buffer[length] = '\0';
+    if(error_read != NULL) {
+        g_warning("extension:cannot open collection : %s", error_read->message);
+        g_clear_error(&error_read);
     }
 
-    return buffer;
+    return jsc_value_new_string(context, content);
 }
 
-static JSCValue * WebAppInterface_openCollection(
-    WebKitWebExtension *extension
-) {
-    g_assert(WEBKIT_IS_WEB_EXTENSION(extension));
+static JSCValue* javascriptinterface_save_collection(WebKitWebExtension *extension, const char *collect_content) {
 
-    // todo: use gtk_file_picker ; cannot use gtk here
-    // try to use DBUS connection between app/extension if possible ??
+    JSCContext *context = jsc_context_get_current();
 
-GList *gtk_parents = gtk_window_list_toplevels();
-GList *l;
-int i =5;
-
-for (l = gtk_parents; l != NULL; l = l->next)
-{
-i++;
-  }
-
-  char str[80];
-
-sprintf(str, " Echo : %i", i);
-
-puts(str);
-g_message(str);
+    bool success = FALSE;
 
 
 
+    return jsc_value_new_boolean(context, success);
+}
+
+static JSCValue* javascriptinterface_get_poster(WebKitWebExtension *extension, const char *poster_name) {
+
+    JSCContext *context = jsc_context_get_current();
+
+    char *filename = NULL;
 
 
 
+filename = "sqdqsdqq";
 
 
-    //  g_assert(WEBKIT_IS_WEB_PAGE(webPage));
+    return jsc_value_new_string(context, filename);
+}
+
+static JSCValue* javascriptinterface_save_poster(WebKitWebExtension *extension, const char *poster_name, const char *poster_content) {
+
+    JSCContext *context = jsc_context_get_current();
+
+    bool success = FALSE;
 
 
 
-    // JSCContext *context = jsc_context_get_current();
-
-    // char *content = readFile("/home/fox/Bureau/vscode");
-
-    // return jsc_value_new_string(context, content);
+    return jsc_value_new_boolean(context, success);
 }
 
 
 
 
 
-
-static void window_object_cleared_callback(
+static void webkit_world_window_cleared_callback(
     WebKitScriptWorld *world,
     WebKitWebPage *page,
     WebKitFrame *frame,
-    WebKitWebExtension *extension //gpointer user_data
+    WebKitWebExtension *extension // user_data
 ) {
-    g_message("extension:window_object_cleared_callback");
-
     g_assert(WEBKIT_IS_WEB_EXTENSION(extension));
 
     // extend javascriptCore APi
@@ -94,187 +75,101 @@ static void window_object_cleared_callback(
 
     JSCClass *js_class = jsc_context_register_class(jsc_context, "WebkitgtkInterface", NULL, NULL, NULL);
 
+    // jsc_class_add_property(js_class, "version", G_TYPE_INT, )
+
     jsc_class_add_method(
-        js_class,
-        "openCollection",
-        G_CALLBACK(WebAppInterface_openCollection), NULL, NULL,
+        js_class, "openCollection",
+        G_CALLBACK(javascriptinterface_open_collection), NULL,
+        NULL, // destroy_notify
         JSC_TYPE_VALUE, //G_TYPE_STRING // return type
         0, G_TYPE_NONE // args number and types
     );
 
-    // jsc_class_add_method(
-    //     js_class,
-    //     "getPoster",
-    //     G_CALLBACK(WebAppInterface_openCollection), NULL, NULL,
-    //     G_TYPE_STRING,
-    //     1, G_TYPE_STRING
-    // );
+    jsc_class_add_method(
+        js_class, "saveCollection",
+        G_CALLBACK(javascriptinterface_save_collection), NULL,
+        NULL, // destroy_notify
+        JSC_TYPE_VALUE, // return type
+        1, G_TYPE_STRING // args number and types
+    );
 
+    jsc_class_add_method(
+        js_class, "getPoster",
+        G_CALLBACK(javascriptinterface_get_poster), NULL,
+        NULL, // destroy_notify
+        JSC_TYPE_VALUE, // return type
+        1, G_TYPE_STRING // args number and types
+    );
+
+    jsc_class_add_method(
+        js_class, "savePoster",
+        G_CALLBACK(javascriptinterface_save_poster), NULL,
+        NULL, // destroy_notify
+        JSC_TYPE_VALUE, // return type
+        2, G_TYPE_STRING, G_TYPE_STRING // args number and types
+    );
 
     JSCValue *js_interface = jsc_value_new_object(jsc_context, extension, js_class);
 
-    jsc_value_object_set_property (jsc_globalObject, "WebkitgtkInterface", js_interface);
-    g_object_unref (js_interface);
-
-
-
-
-
-
-//https://webkitgtk.org/reference/jsc-glib/stable/JSCContext.html#jsc-context-register-class
-
-    // JSGlobalContextRef context = webkit_frame_get_javascript_context_for_script_world(frame, world);
-
-    // JSObjectRef globalObject = JSContextGetGlobalObject(context);
-
-	// JSObjectRef webAppInterfaceObject = JSObjectMake(context, WebAppInterface(), NULL);
-
-	// JSObjectSetProperty(
-    //     context, globalObject,
-    //     JSStringCreateWithUTF8CString("WebkitgtkInterface"),
-    //     webAppInterfaceObject,
-    //     kJSPropertyAttributeNone,
-    //     NULL
-    // );
-    //jsc_value_to_string
+    jsc_value_object_set_property(jsc_globalObject, "WebkitgtkInterface", js_interface);
+    
+    g_object_unref(js_interface);
 }
 
-static void web_page_created_callback (
-    WebKitWebExtension *extension,
-    WebKitWebPage *web_page,
-    gpointer user_data
-) {
-    g_message("extension:web_page_created_callback");
 
+static void web_page_document_loaded_callback(WebKitWebPage *web_page) {
+
+    g_message("extension:Page %" G_GUINT64_FORMAT " created for %s",
+        webkit_web_page_get_id(web_page),
+        webkit_web_page_get_uri(web_page)
+    );
+}
+
+static gboolean web_page_send_request_callback(
+    WebKitWebPage *web_page,
+    WebKitURIRequest *request,
+    WebKitURIResponse *redirected_response
+) {
+    const char* request_method = webkit_uri_request_get_http_method(request);
+    const char* request_uri = webkit_uri_request_get_uri(request);
+
+    g_message("extension:request %.0s %s", request_method, request_uri);
+
+    return FALSE; // continue request
+}
+
+static void web_page_console_message_callback(WebKitWebPage *web_page, WebKitConsoleMessage *console_message) {
+
+    g_message("extension:console #%i - %s",
+        webkit_console_message_get_line(console_message),
+        webkit_console_message_get_text(console_message)
+    );
+}
+
+
+static void web_page_created_callback(WebKitWebExtension *extension, WebKitWebPage *web_page) {
+
+    g_signal_connect(web_page, "console-message-sent", G_CALLBACK(web_page_console_message_callback), NULL);
+    g_signal_connect(web_page, "document-loaded", G_CALLBACK(web_page_document_loaded_callback), NULL);
+    g_signal_connect(web_page, "send-request", G_CALLBACK(web_page_send_request_callback), NULL);
+}
+
+
+
+// main
+G_MODULE_EXPORT void webkit_web_extension_initialize_with_user_data(WebKitWebExtension *extension, GVariant *user_data) {
+    
     g_assert(WEBKIT_IS_WEB_EXTENSION(extension));
 
+    const int unique_id;
+    const char *webextension_dir;
 
-}
-
-
-G_MODULE_EXPORT void webkit_web_extension_initialize_with_user_data(
-    WebKitWebExtension *extension,
-    GVariant *user_data
-) {
-
-     g_assert(WEBKIT_IS_WEB_EXTENSION(extension));
-
-    WebKitScriptWorld *webkit_world = webkit_script_world_get_default();
+    g_variant_get(user_data, "(is)", &unique_id, &webextension_dir);
 
 
-
-
-// if(parent_window == NULL){
-//     g_message("NULL");
-// }
-
-
-
-
-// GtkWidget *dialog;
-// GtkFileChooserAction action = GTK_FILE_CHOOSER_ACTION_OPEN;
-// gint res;
-
-// dialog = gtk_file_chooser_dialog_new ("Open File",
-//                                       parent_window,
-//                                       action,
-//                                       "_Cancel",
-//                                       GTK_RESPONSE_CANCEL,
-//                                       "_Open",
-//                                       GTK_RESPONSE_ACCEPT,
-//                                       NULL);
-
-// res = gtk_dialog_run (GTK_DIALOG (dialog));
-
-
-
-
-
-
+    WebKitScriptWorld *webkit_world = webkit_script_world_get_default(); //webkit_script_world_new_with_name
 
     g_signal_connect(extension, "page-created", G_CALLBACK(web_page_created_callback), NULL);
-
-    g_signal_connect(webkit_world, "window-object-cleared", G_CALLBACK(window_object_cleared_callback), extension);
-
-// G_TYPE_OBJECT, //G_TYPE_FROM_CLASS (extension),
-    // guint extension_ready_signal = g_signal_new(
-    //     "extension:ready",
-    //     G_TYPE_FROM_CLASS(NULL),
-    //     G_SIGNAL_RUN_LAST | G_SIGNAL_NO_RECURSE | G_SIGNAL_NO_HOOKS, //signal_flags
-    //     0, //class_offset
-    //     NULL, //accumulator
-    //     NULL, //accu_data
-    //     NULL, //c_marshaller
-    //     G_TYPE_NONE, //return_type G_TYPE_STRING, G_TYPE_VALUE
-    //     0, //n_params
-    //     NULL // params
-    // );
-
-    // g_signal_emit(extension, extension_ready_signal, 0);
-
-    // g_signal_emit_by_name(extension, "extensionloaded");
+    g_signal_connect(webkit_world, "window-object-cleared", G_CALLBACK(webkit_world_window_cleared_callback), extension);
 }
 
-
-
-
-
-
-
-
-
-
-/*
-
-JSObjectRef WebAppInterface_callAsConstructor(
-    JSContextRef context,
-    JSObjectRef constructor,
-    size_t argumentCount,
-    const JSValueRef arguments[],
-    JSValueRef* exception
-) {
-
-}
-
-void WebAppInterface_finalize(JSObjectRef object) {
-
-}
-
-JSValueRef WebAppInterface_openCollection(
-    JSContextRef context,
-    JSObjectRef function,
-    JSObjectRef object,
-    size_t argumentCount,
-    const JSValueRef arguments[],
-    JSValueRef* exception
-) {
-    JSStringRef response = JSStringCreateWithUTF8CString("{\"ok\":123}");
-
-    return JSValueMakeString(context, response);
-}
-
-JSClassRef WebAppInterface () {
-
-    JSClassDefinition classDefinition = kJSClassDefinitionEmpty;
-
-    static JSStaticFunction staticFunctions[] = {
-        { "openCollection", WebAppInterface_openCollection, kJSPropertyAttributeReadOnly | kJSPropertyAttributeDontDelete },
-        // { "saveCollection", WebAppInterface_saveCollection, kJSPropertyAttributeReadOnly | kJSPropertyAttributeDontDelete },
-        // { "getPoster", WebAppInterface_getPoster, kJSPropertyAttributeReadOnly | kJSPropertyAttributeDontDelete },
-        // { "savePoster", WebAppInterface_savePoster, kJSPropertyAttributeReadOnly | kJSPropertyAttributeDontDelete },
-        { 0, 0, 0 }
-    };
-
-    classDefinition.className = "WebkitgtkInterface";
-    classDefinition.attributes = kJSClassAttributeNone;
-    classDefinition.staticFunctions = staticFunctions;
-    // classDefinition.staticValues = staticValues;
-    classDefinition.finalize = WebAppInterface_finalize;
-    // classDefinition.callAsConstructor = WebAppInterface_callAsConstructor;
-
-    JSClassRef bridge_class = JSClassCreate(&classDefinition);
-
-    return bridge_class;
-}
-
-*/
