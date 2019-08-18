701a4415e4f897dd74b4df2d2f55d43aecf75302
rivetchip <me@spidery.fr>
2019-08-18 19:20:54 +0200

    add main

---
+++ gtkapplication/main.c
@@ -0,0 +1,29 @@
+
+#include <config.h>
+#include "moviecollection.h"
+#include "application.h"
+
+
+int main(int argc, char *argv[]) {
+
+    #if PACKAGE_DEVELOPER_MODE
+        MESSAGE("DEV MODE");
+        // inspector debug
+        putenv("GTK_DEBUG=fatal-warnings");
+        putenv("GOBJECT_DEBUG=instance-count");
+        // putenv("G_ENABLE_DIAGNOSTIC=1");
+    #endif
+
+    int status;
+
+    MovieApplication *app = movie_application_new(
+        PACKAGE_APPLICATION_ID,
+        G_APPLICATION_FLAGS_NONE //| G_APPLICATION_HANDLES_COMMAND_LINE | G_APPLICATION_HANDLES_OPEN
+    );
+
+    status = g_application_run(G_APPLICATION(app), argc, argv);
+
+    g_object_unref(app);
+
+    return status;
+}
