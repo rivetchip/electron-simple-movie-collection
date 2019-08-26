de747c7c8a4e2f8014c06c37575b27ab5dfb3942
rivetchip <me@spidery.fr>
2019-08-26 21:30:55 +0200

    opaque vector and bool

--- gtkapplication/csources/vector.c
+++ gtkapplication/csources/vector.c
@@ -1,11 +1,16 @@
 //Adaptation of Edd Mann's implementation of a simple vector
 //http://eddmann.com/posts/implementing-a-dynamic-vector-array-in-c/
-
 #include "vector.h"
-
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
+
+// type definition
+struct _vector {
+    void **items;
+    unsigned int capacity;
+    unsigned int total;
+};
 
 vector *vector_new() {
     vector *v = malloc(sizeof(*v));
@@ -26,28 +31,30 @@ static int vector_resize(vector *v, int capacity) {
     if((items = realloc(v->items, sizeof(void*) * capacity)) != NULL) {
         v->items = items;
         v->capacity = capacity;
-        return 1;
+        return true;
     }
-    return 0;
+    return false;
 }
 
-int vector_add(vector *v, void *item) {
+bool vector_add(vector *v, void *item) {
     if(v->capacity == v->total) {
         if(!vector_resize(v, v->capacity * 2)) {
-            return 0;
+            return false;
         }
     }
     v->items[v->total++] = item;
 
-    return (v->total - 1); // return last key
+    // *index = (v->total - 1); // return last key
+
+    return true;
 }
 
-int vector_set(vector *v, int index, void *item) {
+bool vector_set(vector *v, int index, void *item) {
     if(index >= 0 && index < v->total) {
         v->items[index] = item;
-        return 1;
+        return true;
     }
-    return 0;
+    return false;
 }
 
 void *vector_get(vector *v, int index) {
@@ -57,9 +64,9 @@ void *vector_get(vector *v, int index) {
     return NULL;
 }
 
-int vector_delete(vector *v, int index) {
+bool vector_delete(vector *v, int index) {
     if(index < 0 || index >= v->total) {
-        return 0;
+        return false;
     }
 
     v->items[index] = NULL;
@@ -74,10 +81,10 @@ int vector_delete(vector *v, int index) {
     // if quarter full the contents is reallocated to a vector of half the current size
     if(v->total > 0 && v->total == v->capacity / 4) {
         if(!vector_resize(v, v->capacity / 2)) {
-            return 0;
+            return false;
         }
     }
-    return 1;
+    return true;
 }
 
 void vector_free(vector *v) {
--- gtkapplication/csources/vector.h
+++ gtkapplication/csources/vector.h
@@ -1,22 +1,21 @@
 #pragma once
 
+#include <stdbool.h>
+
 #ifdef  __cplusplus
     extern "C" {
 #endif
 
+// type definition
+typedef struct _vector vector;
 
-typedef struct vector {
-    void **items;
-    unsigned int capacity;
-    unsigned int total;
-} vector;
-
+// public functions
 vector *vector_new();
 int vector_total(vector *v);
-int vector_add(vector *v, void *item);
-int vector_set(vector *v, int index, void *item);
+bool vector_add(vector *v, void *item);
+bool vector_set(vector *v, int index, void *item);
 void *vector_get(vector *v, int index);
-int vector_delete(vector *v, int index);
+bool vector_delete(vector *v, int index);
 void vector_free(vector *v);
 
 
